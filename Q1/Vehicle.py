04b10435c59e14fc0723a03a03ba1746fec6d11f
daniellegaspi1 <dlegaspi2030@clc.pshs.edu.ph>
2026-09-18 08:15:53 +0800

    Create Vehicle.py

---
+++ Q1/Vehicle.py
@@ -0,0 +1,28 @@
+class Vehicle:
+    def __init__(self,kindofvehicle):
+        self.kindofvehicle = kindofvehicle
+        print(self.kindofvehicle,"created")
+    def move(self,distance):
+        print(self.kindofvehicle,"moved",distance)
+
+class Car(Vehicle):
+    def __init__(self,kindofvehicle,brand,model):
+        super().__init__(kindofvehicle)
+        print("It is a",brand,model)
+    def move(self,distance):
+        super().move(distance)
+        print("KM")
+
+class Boat(Vehicle):
+    def __init__(self,kindofvehicle,model):
+        self.model = model
+        super().__init__(kindofvehicle)
+        print("It is a",model)
+    def move(self, distance):
+        super().move(distance)
+        print("Nm")
+        
+vios = Car("car", "Toyota", "Vios")
+vios.move(10)
+ferry = Boat("ferry","SuperFerry")
+ferry.move(20)
